2ebef3bb80a0479c68f72d296c30dfccb4c69222
Aditya <walker310407@gmail.com>
2024-11-06 18:31:18 +0530

    Apps Updated

--- backend/customer/urls.py
+++ backend/customer/urls.py
@@ -1,7 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import get_customer_profile
+from .views import get_customer_profile,login,signup,logout
 
 urlpatterns = [
-    path('profile/', get_customer_profile, name='get_customer_profile'),
+    path('profile', get_customer_profile, name='get_customer_profile'),
+    path('login', login,name='login'),
+    path('signup',signup,name='signup'),
+    path('logout',logout,name='LogOut')
 ]
